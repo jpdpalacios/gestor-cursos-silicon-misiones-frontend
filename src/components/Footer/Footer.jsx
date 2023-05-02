791efb18a320ff6f034f53105a4d03a56c81b45a
jpdpalacios <jpdpalacios.dev@gmail.com>
2023-05-02 01:00:47 -0300

    add: [Footer.jsx/css]

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">© 2023 - Silicon Misiones</p>
+    </footer>
+  );
+}
+
+export default Footer;
