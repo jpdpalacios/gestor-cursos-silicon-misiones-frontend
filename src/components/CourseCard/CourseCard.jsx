cfa6693ff60f447e1f78c449e9d8446ec28fa882
jpdpalacios <jpdpalacios.dev@gmail.com>
2023-05-22 14:35:09 -0300

    mod: [CourseCard]

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CourseCard.css';
-import imgTest from '../../assets/img/image-course-card-test.png'
+import imgDefault from '../../assets/img/image-course-default.png'
 
-function CourseCard() {
+function CourseCard({ name, image, year, description }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [clase, setClase] = useState('course-card-name');
+
+  const handleCardClick = () => {
+    setIsExpanded(!isExpanded);
+    setClase(clase === 'course-card-name' ? 'new-course-card-name' : 'course-card-name');
+  };
   return (
     <div className="CourseCard">
-        <div className="course-card-image">
-            <img src={imgTest} alt="" />
-            <p>Virtual</p>
+      <div className="course-card-image">
+        <img src={image === "" || image === null ? imgDefault : image} alt="" />
+        <p>{year}</p>
+      </div>
+      <div className={clase} onClick={handleCardClick}>
+        <h5>{name}</h5>
+        <div className={`card-content ${isExpanded ? 'expanded' : 'collapsed'}`}>
+          <p>{description}</p>
         </div>
-            <div className="course-card-name">
-            <h5>Desarrollo Web Full Stach con Java</h5>
-            </div>
-    </div>
+      </div>
+    </div >
   );
 }
 
